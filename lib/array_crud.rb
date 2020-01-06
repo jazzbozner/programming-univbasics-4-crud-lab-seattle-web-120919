926c79714dcacbf2c6aed598a4c7ba6f737f5c1d
Jazz Bozner <jazzbozner@gmail.com>
2020-01-06 23:49:01 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -34,6 +34,6 @@ def retrieve_last_element_from_array(arr)
   arr.last
 end
 
-def update_element_from_index(arr, element)
-  arr[element] = element
+def update_element_from_index(arr, idex, element)
+  arr[index] = element
 end
